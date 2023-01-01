e77591badfe2c60ebd6a0968671f5787482b949d
MariaESLIW <121664848+MariaESLIW@users.noreply.github.com>
2023-01-01 16:58:58 +0200

    solution

---
+++ hw4point/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace hw4point
+{
+	public class Point
+	{
+		int x;
+		int y;
+
+		public Point(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public void printPoint()
+		{
+			Console.WriteLine("coordinate x = " + this.x + ", coordinate y = " + this.y);
+		}
+	}
+}
+
---
+++ hw4point/Program.cs
@@ -0,0 +1,19 @@
+﻿
+hw4point.Point[] points = new hw4point.Point[10];
+
+points[0] = new hw4point.Point(5, 7);
+points[1] = new hw4point.Point(3, 7);
+points[2] = new hw4point.Point(-5, -7);
+points[3] = new hw4point.Point(-3, -7);
+points[4] = new hw4point.Point(7, 9);
+points[5] = new hw4point.Point(5, 9);
+points[6] = new hw4point.Point(-7, -9);
+points[7] = new hw4point.Point(-5, -9);
+points[8] = new hw4point.Point(1, 7);
+points[9] = new hw4point.Point(-1, -7);
+
+for(int i=0; i<points.Length; i++)
+{
+    points[i].printPoint();
+}
+
